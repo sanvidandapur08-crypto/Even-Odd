35ba67290ca7671d6c1e6acbf7c354e53faeffde
sanvidandapur08-crypto <sanvidandapur08@gmail.com>
2026-09-11 15:48:01 +0530

    Update Even-odd.py

--- Even-odd.py
+++ Even-odd.py
@@ -2,5 +2,7 @@ num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print(num, "is an Even number.")
+    print("The number is divisble by 2")
 else:
     print(num, "is an Odd number.")
+    print("The number is not divisble by 2")
